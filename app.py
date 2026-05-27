441a38721156701fc92569bba96f007186e89d36
avi-pal <avirup150@gmail.com>
2026-05-27 16:07:15 +0530

    update api key

--- app.py
+++ app.py
@@ -8,9 +8,19 @@ import os
 # -------------------------
 load_dotenv()
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+from groq import Groq
+from dotenv import load_dotenv
+import streamlit as st
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["GROQ_API_KEY"]
+
+client = Groq(api_key=api_key)
 
 # -------------------------
 # Page Config
